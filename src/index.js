1c7f22efde6d2942a738da96d6cfc5ff7b85c581
Alex Gap <alex.gap@consensys.net>
2020-06-06 10:36:13 -0700

    Add ethers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { InfuraProvider } from "@ethersproject/providers";
+const provider = new InfuraProvider(
+  "ropsten",
+  "780797e7832148289ef283af4ade0a58"
+);
+provider.getBlockNumber().then(console.log);
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
